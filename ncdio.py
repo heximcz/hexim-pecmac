490af891a598658b1ce38b9a4cb5eff7f7765379
Lubomir Spacek <admin@best-net.cz>
2019-04-24 23:33:08 +0200

    add try block

--- ncdio.py
+++ ncdio.py
@@ -1,4 +1,8 @@
 from NcdIoCurrent import NcdIo
 
-dev = NcdIo(42)
-dev.board_info()
+i2c_address = 42
+dev = NcdIo(i2c_address)
+try:
+    dev.board_info()
+except IOError:
+    print("No device exist on address: %d" % i2c_address)
