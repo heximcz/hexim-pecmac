932c520dba2d7c654fed8f4cc521603607a98374
Lubomir Spacek <admin@best-net.cz>
2019-04-25 17:37:19 +0200

    add firmware rev. and read calibration data

--- NcdIoCurrent/NcdIo.py
+++ NcdIoCurrent/NcdIo.py
@@ -10,11 +10,25 @@ from smbus2 import SMBus
 
 class NcdIo:
     """
-    Initialize i2c address and get board information.
+    Get values from PECMAC current board
 
     :param address: i2c address
     :type address: int
+    :param volts: single phase voltage
+    :type volts: int
     """
+    # Type of Sensor
+    board = 0
+    # Maximum Current
+    max_current = 0
+    # No. of Channels
+    channels = 0
+    # firmware revision
+    firmware = 0
+    # i2c address
+    _address = 0
+    # single phase volts
+    _volts = 0
 
     def __init__(self, address, volts):
         # create SMBus
@@ -25,26 +39,6 @@ class NcdIo:
         self._volts = volts
         # get board parameters
         self._ident_sensors()
-
-    def _ident_sensors(self):
-        # Command for reading device identification data
-        # 0x6A(106), 0x02(2), 0x00(0),0x00(0), 0x00(0) 0x00(0), 0xFE(254)
-        # Header byte-2, command-2, byte 3, 4, 5 and 6 are reserved, checksum
-        cmd = [106, 2, 0, 0, 0, 0]
-        register = 146
-        cmd.append(self.__checksum(register, cmd))
-        self.__write_block(self._address, register, cmd)
-        time.sleep(0.5)
-
-        # Read data back from 0x55(85), 3 bytes
-        # Type of Sensor, Maximum Current, No. of Channels
-        data = self.__read_block(self._address, 85, 3)
-        # Type of Sensor
-        self.board = data[0]
-        # Maximum Current
-        self.max_current = data[1]
-        # No. of Channels
-        self.channels = data[2]
 
     def read_current(self):
         # Command for reading current
@@ -60,7 +54,7 @@ class NcdIo:
         # current MSB1, current MSB, current LSB
         return self.__read_block(self._address, 85, self.channels * 3)
 
-    def print_all_currents(self):
+    def print_currents(self):
         """ print readable data for all sensors """
         data = self.read_current()
         for i in range(0, self.channels):
@@ -70,7 +64,7 @@ class NcdIo:
             # Output data to screen
             print("Channel no : %d " % (i + 1))
             print("Current Value : %.3f A" % current)
-            print("Watt Value : %.3f W" % current * self._volts)
+            print("Watt Value : %.3f W" % (current * self._volts))
 
     def get_one_current(self, number):
         """ get one current in single value first channel is 0 """
@@ -78,17 +72,55 @@ class NcdIo:
             data = self.read_current()
             return self.__compute_current(number, data)
 
+    def read_calibration_values(self):
+        # Command for reading calibration values
+        # 146,106,3,1,3,0,0,3
+        # Header byte-2, command-1, start channel-1, stop channel-12, byte 5 and 6 reserved, checksum
+        cmd = [106, 3, 1, self.channels, 0, 0]
+        register = 146
+        cmd.append(self.__checksum(register, cmd))
+        self.__write_block(self._address, register, cmd)
+        time.sleep(0.5)
+
+        # Read data back from 0x55(85), No. of Channels * 3 bytes
+        # current MSB1, current MSB, current LSB
+        return self.__read_block(self._address, 85, self.channels * 2)
+
+    def print_calibration(self):
+        """ print calibration data for all sensors """
+        data = self.read_calibration_values()
+        for i in range(0, self.channels):
+            # Convert the data
+            msb = data[i * 2]
+            lsb = data[1 + i * 2]
+            calibration = (msb * 256 + lsb)
+
+            # Output data to screen
+            print("Channel no : %d " % (i + 1))
+            print("Calibration Value : %.3f A" % calibration)
+
     def board_info(self):
         print("Type of Sensor : %d" % self.board)
         print("Maximum Current : %d A" % self.max_current)
         print("No. of Channels : %d" % self.channels)
 
-    @staticmethod
-    def __compute_current(idx, data):
-        msb1 = data[idx * 3]
-        msb = data[1 + idx * 3]
-        lsb = data[2 + idx * 3]
-        return (msb1 * 65536 + msb * 256 + lsb) / 1000.0
+    def _ident_sensors(self):
+        # Command for reading device identification data
+        # 0x6A(106), 0x02(2), 0x00(0),0x00(0), 0x00(0) 0x00(0), 0xFE(254)
+        # Header byte-2, command-2, byte 3, 4, 5 and 6 are reserved, checksum
+        cmd = [106, 2, 0, 0, 0, 0]
+        register = 146
+        cmd.append(self.__checksum(register, cmd))
+        self.__write_block(self._address, register, cmd)
+        time.sleep(0.5)
+
+        # Read data back from 0x55(85), 3 bytes
+        # Type of Sensor, Maximum Current, No. of Channels
+        data = self.__read_block(self._address, 85, 4)
+        self.board = data[0]
+        self.max_current = data[1]
+        self.channels = data[2]
+        self.firmware = data[3]
 
     def __write_block(self, address, register, cmd):
         try:
@@ -103,6 +135,13 @@ class NcdIo:
         except OSError as e:
             print("Write block error: %s" % e)
             sys.exit(os.EX_OSERR)
+
+    @staticmethod
+    def __compute_current(idx, data):
+        msb1 = data[idx * 3]
+        msb = data[1 + idx * 3]
+        lsb = data[2 + idx * 3]
+        return (msb1 * 65536 + msb * 256 + lsb) / 1000.0
 
     @staticmethod
     def __checksum(register, cmd):
--- ncdio.py
+++ ncdio.py
@@ -4,4 +4,5 @@ i2c_address = 42
 volts = 230
 dev = NcdIo(i2c_address, volts)
 dev.board_info()
-dev.print_all_currents()
+dev.print_currents()
+dev.print_calibration()
