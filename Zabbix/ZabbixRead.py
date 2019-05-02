5d5d0a46bc80b8bc63f17a51087e77a925d10ac6
Lubomir Spacek <admin@best-net.cz>
2019-05-02 15:54:11 +0200

    check no. of channels

--- Zabbix/ZabbixRead.py
+++ Zabbix/ZabbixRead.py
@@ -6,15 +6,20 @@ class ZabbixRead:
     def __init__(self):
         zbx = ZabbixFile()
         self.data = zbx.read()
+        self.sensors = len(self.data)
 
     def get_current(self, phase):
         # 'F1': {'ampere': '0.00', 'watt': '0.00'},
         # 'F2': {'ampere': '0.00', 'watt': '0.00'},
         # 'F3': {'ampere': '0.00', 'watt': '0.00'}}}
-        return self.data['F'+str(phase)]['ampere']
+        # "No. of channel is over range."
+        if phase <= self.sensors:
+            return self.data['F'+str(phase)]['ampere']
+        return "Phase number is over the range."
 
     def get_watts(self, phase):
         # 'F1': {'ampere': '0.00', 'watt': '0.00'},
         # 'F2': {'ampere': '0.00', 'watt': '0.00'},
         # 'F3': {'ampere': '0.00', 'watt': '0.00'}}}
-        return self.data['F'+str(phase)]['watt']
+        if phase <= self.sensors:
+            return self.data['F'+str(phase)]['watt']
